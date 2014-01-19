c6c8ee27536b7b40c2388b1b77dd2247bf907889
jkeef321 <justinkeefer@Justins-MacBook-Pro.local>
2014-01-18 19:25:32 -0500

    the start of adding the start game layer

---
+++ Forest Defense/StartGameLayer.h
@@ -0,0 +1,20 @@
+//
+//  StartGameLayer.h
+//  Forest Defense
+//
+//  Created by Justin Keefer on 1/18/14.
+//  Copyright (c) 2014 Justin Keefer. All rights reserved.
+//
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// StartGameLayer
+@interface StartGameLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the StartGameLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ Forest Defense/StartGameLayer.m
@@ -0,0 +1,13 @@
+//
+//  StartGameLayer.m
+//  Forest Defense
+//
+//  Created by Justin Keefer on 1/18/14.
+//  Copyright (c) 2014 Justin Keefer. All rights reserved.
+//
+
+#import "StartGameLayer.h"
+
+@implementation StartGameLayer
+
+@end
